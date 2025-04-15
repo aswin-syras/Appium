9836bcec5561cda5724bac3f48f569f3822389e1
Aswin Syras <47082111+aswin-syras@users.noreply.github.com>
2025-04-14 22:32:36 -0400

    Initial commit

---
+++ src/main/java/AppiumVerity/AppiumVerity/App.java
@@ -0,0 +1,13 @@
+package AppiumVerity.AppiumVerity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
